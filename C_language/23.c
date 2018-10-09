6df24127b8dff664d9b41be2720eeaee004d24ca
shin99921 <35947693+shin99921@users.noreply.github.com>
2018-10-09 22:54:57 +0900

    Add files via upload

--- C_language/23.c
+++ C_language/23.c
@@ -5,8 +5,16 @@ int main() {
 	int H,W,N;
 
 	scanf("%d %d %d",&H,&W,&N);
-
-	printf("%d\n",((N%H)*100)+(N/H+1));
+	
+	if (N%H == 0)
+	{
+		printf("%d",(H*100)+(N/H));
+	}
+		
+	else
+	{
+		printf("%d\n",((N%H)*100)+(N/H+1));
+	}
 
 	return 0;
 
