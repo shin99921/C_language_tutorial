3fc4f042a036c81087eae6ac8b69991650109115
shin99921 <35947693+shin99921@users.noreply.github.com>
2018-12-05 12:49:49 +0900

    Add files via upload

--- test_example/72.c
+++ test_example/72.c
@@ -1,35 +1,21 @@
 #include <stdio.h>
 
-unsigned long long F (int n);
+int F (int);
 
-int main() {
+int main () {
 	
 	int n;
 	
 	while (scanf("%d",&n) == 1 && n >= 0)
-		printf("#%d: %llu\n",n,F(n));
+		printf("#%d: %d\n",n,F(n));
 		
 	return 0;
 }
 
-unsigned long long F (int n)
+int F (int n)
 {
-	unsigned long long tmp,f=1,o=1,c=3;
-	
-	if (n == 0)
-		return 0;
-		
-	else if (n <= 2)
-		return 1;
-		
+	if (n < 2)
+		return n;
 	else
-		while (c <= n)
-		{
-			tmp=o;
-			o+=f;
-			f=tmp;
-			c++;
-		}
-		
-	return o;
+		return F(n-2) + F(n-1);
 }
